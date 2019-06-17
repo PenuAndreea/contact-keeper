9be4934eb3831c6620f1a7e3305b3a9b288b9215
Andreea <andreea@selma.ai>
2019-06-17 14:37:00 +0200

    add jwt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/User')
@@ -50,7 +52,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt)
       // save to database
       await user.save()
-      res.send('User saved')
+      // object we want to send in the token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 }, // 3600 = 1 hour
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send('Server error')
