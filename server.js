0d3392603e5b470d91fbfd7f99d0978841d5a0a5
Andreea <andreea@selma.ai>
2019-06-17 13:53:43 +0200

    add validation

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express()
 // Connect Database
 connectDB()
 
+// Init Middleware
+// by doing this we can now accept data, and don't need body parser
+app.use(
+  express.json({
+    extended: false
+  })
+)
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the contact keeper API...' })
 )
