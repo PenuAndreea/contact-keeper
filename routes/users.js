d96af33c408769bf93f745026a5248a2fc874914
Andreea <andreea@selma.ai>
2019-06-17 14:22:11 +0200

    check if user is unique before adding him

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/User')
@@ -20,13 +21,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body
+
+    // check if the user already exists
+    try {
+      let user = await User.findOne({ email })
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' })
+      }
+
+      // Create a new user
+      user = new User({
+        name,
+        email,
+        password
+      })
+
+      // encrypt the password
+      // 10 is the default: how safe the password is
+      const salt = await bcrypt.genSalt(10)
+      // gives us a hash version of the password
+      user.password = await bcrypt.hash(password, salt)
+      // save to database
+      await user.save()
+      res.send('User saved')
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
   }
 )
 
