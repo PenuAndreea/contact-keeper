6f83ea52b5925ba483ebddbfd9eee1b6a31d4f6a
Andreea <andreeapenu.work@gmail.com>
2019-06-17 17:47:47 +0200

    update contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -64,8 +64,37 @@ router.post(
 // @route       PUT api/contacts/:id
 // @desc        Update contact
 // @access      Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact')
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body
+
+  // build a contact object
+  // add to the contactFields if the fields are included
+  const contactFields = {}
+  if (name) contactFields.name = name
+  if (email) contactFields.email = email
+  if (phone) contactFields.phone = phone
+  if (type) contactFields.type = type
+
+  try {
+    let contact = await Contact.findById(req.params.id)
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' })
+    // Make sure user owns contact
+    // compare to the user of the token
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' })
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: contactFields
+      },
+      { new: true } // if it doesn't exist, create it
+    )
+    res.json(contact)
+  } catch (err) {
+    console.error(er.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 // @route       DELETE api/contacts/:id
